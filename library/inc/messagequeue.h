148d00138bd855d2fceace6ec6a203afd0afecae
Kees-Jan Dijkzeul <kees-jan@dijkzeul.eu>
2020-10-24 16:45:54 +0200

    Solutions to exercise 1

--- library/inc/messagequeue.h
+++ library/inc/messagequeue.h
@@ -7,5 +7,10 @@
 class MessageQueue
 {
 public:
+  std::optional<std::string> try_read();
+  void                       write(std::string newMessage);
+
 private:
+  std::mutex                 m;
+  std::optional<std::string> message;
 };
